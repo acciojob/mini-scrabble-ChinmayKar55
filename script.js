3f63d12ae0b7e8dc360b954cb82bb613db573f63
Chinmay <chinmaykarjan9@gmail.com>
2022-11-28 20:06:41 +0530

    Solution

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+function myFunction() {
+    var x = document.getElementById("evaluatedText").value
+    console.log(x);
+    
+    document.getElementById("letterCount").innerHTML = x.length;
+}
